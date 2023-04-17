24cc021318b6590cf733698f5dfceb1bf11390dc
Shireen Chand <72601697+shireenchand@users.noreply.github.com>
2023-04-17 23:06:41 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import os,sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from all_apis import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
