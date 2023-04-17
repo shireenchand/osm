74cea9e8e03c9bf3ad0941ae6c095a3f55d9078d
Shireen Chand <72601697+shireenchand@users.noreply.github.com>
2023-04-17 23:08:38 +0530

    Add files via upload

--- osm.py
+++ osm.py
@@ -2,26 +2,29 @@ import requests
 from flask import Flask, request
 import json
 
-app = Flask(__name__)
+def create_app():
 
-@app.route('/generate_text', methods=['POST'])
-def generate_text():
-    # Get the input text from the request
-    input_text = request.get_json()['input_text']
-    API_URL = "https://api-inference.huggingface.co/models/nomic-ai/gpt4all-j"
-    headers = {"Authorization": "Bearer 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"}
+    app = Flask(__name__)
 
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.json()
-        
-    output = query({
-        "inputs": str(input_text),
-        "options":{
-        "wait_for_model":True
-        }
-    })
-    return json.dumps(output[0]['generated_text'])
+    @app.route('/generate_text', methods=['POST'])
+    def generate_text():
+        # Get the input text from the request
+        input_text = request.get_json()['input_text']
+        API_URL = "https://api-inference.huggingface.co/models/nomic-ai/gpt4all-j"
+        headers = {"Authorization": "Bearer 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"}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+            
+        output = query({
+            "inputs": str(input_text),
+            "options":{
+            "wait_for_model":True
+            }
+        })
+        return json.dumps(output[0]['generated_text'])
+
+    return app
+    # if __name__ == '__main__':
+    #     app.run(debug=True)
